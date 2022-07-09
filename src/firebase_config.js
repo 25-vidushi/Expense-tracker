461c689e129e9bba19e47d95e1c10249920787ed
25-vidushi <vidushi232@kgpian.iitkgp.ac.in>
2022-07-09 09:23:48 +0530

    changes

---
+++ src/firebase_config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBW8mUX4c4VpGEmlkTgo6mYWLpfaTOxz6U",
+    authDomain: "expense-tracker-d6dd2.firebaseapp.com",
+    projectId: "expense-tracker-d6dd2",
+    storageBucket: "expense-tracker-d6dd2.appspot.com",
+    messagingSenderId: "809053638637",
+    appId: "1:809053638637:web:6948a4eb79f8fa6506e1e4",
+    measurementId: "G-BQ37V4ZHRH"
+  };
+
+  const app = initializeApp(firebaseConfig);
