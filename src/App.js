70a62326ebf26ff8ed474ee700c051027f4b1380
25-vidushi <vidushi232@kgpian.iitkgp.ac.in>
2022-07-09 09:44:02 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,25 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
-
 import { GlobalProvider } from './context/GlobalState';
-
+import { useAuthState } from 'react-firebase-hooks/auth';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBW8mUX4c4VpGEmlkTgo6mYWLpfaTOxz6U",
+  authDomain: "expense-tracker-d6dd2.firebaseapp.com",
+  projectId: "expense-tracker-d6dd2",
+  storageBucket: "expense-tracker-d6dd2.appspot.com",
+  messagingSenderId: "809053638637",
+  appId: "1:809053638637:web:6948a4eb79f8fa6506e1e4",
+  measurementId: "G-BQ37V4ZHRH"
+})
+
 
 function SignIn() {
 
